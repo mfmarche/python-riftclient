2d8bf517f6459a6f52d0fe788290e51f075128cf
Michael Marchetti <mmarchetti@sandvine.com>
2017-02-21 15:45:13 -0500

    initial checkin

---
+++ riftclient/common/manoRift.py
@@ -0,0 +1,294 @@
+from io import BytesIO 
+import pycurl
+import json
+import pprint
+import uuid
+from prettytable import PrettyTable
+import time
+
+class ManoRift():
+    def __init__(self,host,upload_port=4567):
+        self._user='admin'
+        self._password='admin'
+        self._host=host
+        self._upload_port=upload_port
+        self._url = 'https://{}/'.format(self._host)
+        self._upload_url = 'https://{}:{}/'.format(self._host,self._upload_port)
+
+    def get_curl_cmd(self,url):
+        curl_cmd = pycurl.Curl()
+        curl_cmd.setopt(pycurl.HTTPGET,1)
+        curl_cmd.setopt(pycurl.URL, self._url + url )
+        curl_cmd.setopt(pycurl.SSL_VERIFYPEER,0)
+        curl_cmd.setopt(pycurl.SSL_VERIFYHOST,0)
+        curl_cmd.setopt(pycurl.USERPWD, '{}:{}'.format(self._user,self._password))
+        curl_cmd.setopt(pycurl.HTTPHEADER, ['Accept: application/vnd.yand.data+json','Content-Type": "application/vnd.yang.data+json'])
+        return curl_cmd
+
+    def get_curl_upload_cmd(self,filename):
+        curl_cmd = pycurl.Curl()
+        curl_cmd.setopt(pycurl.HTTPPOST,[(('descriptor',(pycurl.FORM_FILE,filename)))])
+        curl_cmd.setopt(pycurl.URL, self._upload_url + 'api/upload')
+        curl_cmd.setopt(pycurl.SSL_VERIFYPEER,0)
+        curl_cmd.setopt(pycurl.SSL_VERIFYHOST,0)
+        curl_cmd.setopt(pycurl.USERPWD, '{}:{}'.format(self._user,self._password))
+        return curl_cmd
+
+    def get_ns_opdata(self,id):
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('api/operational/ns-instance-opdata/nsr/{}?deep'.format(id))
+        curl_cmd.setopt(pycurl.HTTPGET,1)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        return resp
+
+    def get_ns_catalog(self):
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('api/running/nsd-catalog/nsd')
+        curl_cmd.setopt(pycurl.HTTPGET,1)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        return resp
+
+    def get_ns_instance_list(self):
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('api/running/ns-instance-config')
+        curl_cmd.setopt(pycurl.HTTPGET,1)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        return resp['nsr:ns-instance-config']
+
+    def get_vnf_catalog(self):
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('api/running/vnfd-catalog/vnfd')
+        curl_cmd.setopt(pycurl.HTTPGET,1)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        return resp
+
+    def get_vnfr_catalog(self):
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('v1/api/operational/vnfr-catalog/vnfr')
+        curl_cmd.setopt(pycurl.HTTPGET,1)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        return resp
+
+    def list_key_pair(self):
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('v1/api/config/key-pair?deep')
+        curl_cmd.setopt(pycurl.HTTPGET,1)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        if 'nsr:key-pair' in resp:
+            return resp['nsr:key-pair']
+        return list()
+
+    def list_ns_catalog(self):
+        resp = self.get_ns_catalog()
+        table=PrettyTable(['NSD Name','Id'])
+        for ns in resp['nsd:nsd']:
+            table.add_row([ns['name'],ns['id']])
+        table.align='l'
+        print(table)
+
+    def list_ns_instance(self):
+        resp = self.get_ns_instance_list()
+        table=PrettyTable(['NS Instance Name','Id','Catalog Name','Operational Status','Config Status'])
+        if 'nsr' in resp:
+            for ns in resp['nsr']:
+                nsopdata=self.get_ns_opdata(ns['id'])
+                nsr=nsopdata['nsr:nsr']
+                table.add_row([nsr['name-ref'],nsr['ns-instance-config-ref'],nsr['nsd-name-ref'],nsr['operational-status'],nsr['config-status']])
+        table.align='l'
+        print(table)
+
+    def get_nsd(self,name):
+        resp = self.get_ns_catalog()
+        for ns in resp['nsd:nsd']:
+           if name == ns['name']:
+               return ns
+        return None 
+
+    def get_ns(self,name):
+        resp=self.get_ns_instance_list()
+        for ns in resp['nsr']:
+           if name == ns['name']:
+               return ns
+        return None 
+    
+    def instantiate_ns(self,nsd_name,nsr_name,account,vim_network_prefix=None,ssh_keys=None,description='default description',admin_status='ENABLED'):
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('api/config/ns-instance-config')
+        curl_cmd.setopt(pycurl.POST,1)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+
+        postdata={}
+        postdata['nsr'] = list()
+        nsr={}
+        nsr['id']=str(uuid.uuid1())
+
+        nsd=self.get_nsd(nsd_name)
+        if nsd is None:
+            raise Exception('cannot find nsd {}'.format(nsd_name)) 
+        
+        nsr['nsd']=nsd
+        nsr['name']=nsr_name
+        nsr['short-name']=nsr_name
+        nsr['description']=description
+        nsr['admin-status']=admin_status
+        nsr['cloud-account']=account
+
+        # ssh_keys is comma separate list
+        ssh_keys_format=[]
+        for key in ssh_keys.split(','):
+            ssh_keys_format.append({'key-pair-ref':key})
+
+        nsr['ssh-authorized-key']=ssh_keys_format
+
+        if vim_network_prefix is not None:
+            for index,vld in enumerate(nsr['nsd']['vld']):
+                pprint.pprint(vld)
+                network_name = vld['name']
+                nsr['nsd']['vld'][index]['vim-network-name'] = '{}-{}'.format(vim_network_prefix,network_name)
+
+        pprint.pprint(nsr)
+        postdata['nsr'].append(nsr)
+        jsondata=json.dumps(postdata)
+        curl_cmd.setopt(pycurl.POSTFIELDS,jsondata)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        pprint.pprint(resp)
+
+    def delete_nsd(self,nsd_name):
+        nsd=self.get_nsd(nsd_name)
+        if nsd is None:
+            raise Exception('cannot find nsd {}'.format(nsd_name)) 
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('api/running/nsd-catalog/nsd/'+nsd['id'])
+        curl_cmd.setopt(pycurl.CUSTOMREQUEST, "DELETE")
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        pprint.pprint(resp)
+
+    def terminate_ns(self,ns_name):
+        ns=self.get_ns(ns_name)
+        if ns is None:
+            raise Exception('cannot find ns {}'.format(ns_name)) 
+  
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('api/config/ns-instance-config/nsr/'+ns['id'])
+        curl_cmd.setopt(pycurl.CUSTOMREQUEST, "DELETE")
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        pprint.pprint(resp)
+
+    def upload_package(self,filename):
+        data = BytesIO()
+        curl_cmd=self.get_curl_upload_cmd(filename)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        pprint.pprint(resp)
+      
+    def add_vim_account(self,name,user_name,secret,auth_url,tenant,mgmt_network,float_ip_pool,account_type='openstack'):
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('api/config/cloud')
+        curl_cmd.setopt(pycurl.POST,1)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        vim_account={}
+        vim_account['account']={}
+
+        vim_account['account']['name'] = name
+        vim_account['account']['account-type'] = account_type
+        vim_account['account'][account_type] = {}
+        vim_account['account'][account_type]['key'] = user_name
+        vim_account['account'][account_type]['secret'] = secret
+        vim_account['account'][account_type]['auth_url'] = auth_url
+        vim_account['account'][account_type]['tenant'] = tenant
+        vim_account['account'][account_type]['mgmt-network'] = mgmt_network
+        vim_account['account'][account_type]['floating-ip-pool'] = float_ip_pool
+
+        jsondata=json.dumps(vim_account)
+        curl_cmd.setopt(pycurl.POSTFIELDS,jsondata)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        pprint.pprint(resp)
+
+    def list_vim_accounts(self):
+        data = BytesIO()
+        curl_cmd=self.get_curl_cmd('v1/api/operational/cloud/account')
+        curl_cmd.setopt(pycurl.HTTPGET,1)
+        curl_cmd.setopt(pycurl.WRITEFUNCTION, data.write)
+        curl_cmd.perform() 
+        curl_cmd.close()
+        resp = json.loads(data.getvalue())
+        if 'rw-cloud:account' in resp:
+            return resp['rw-cloud:account']
+        return list()
+
+    def show_ns(self,ns_name):
+        resp = self.get_ns_instance_list()
+        table=PrettyTable(['Attribute','Value'])
+
+        if 'nsr' in resp:
+            for ns in resp['nsr']:
+                if ns_name == ns['name']:
+                    # dump ns config
+                    for k,v in ns.items():
+                        table.add_row([k,json.dumps(v,indent=2)])
+
+                    nsopdata=self.get_ns_opdata(ns['id'])
+                    nsr_optdata=nsopdata['nsr:nsr']
+                    for k,v in nsr_optdata.items():
+                        table.add_row([k,json.dumps(v,indent=2)])
+        table.align='l'
+        print(table)
+
+    def show_ns_scaling(self,ns_name):
+        resp = self.get_ns_instance_list()
+
+        table=PrettyTable(['instance-id','Operational Status','create-time','Vnfr Ids'])
+
+        if 'nsr' in resp:
+            for ns in resp['nsr']:
+                if ns_name == ns['name']:
+                    nsopdata=self.get_ns_opdata(ns['id'])
+                    scaling_records=nsopdata['nsr:nsr']['scaling-group-record']
+                    for record in scaling_records:
+                        instances=record['instance'] 
+                        for inst in instances:
+                            table.add_row([inst['instance-id'],inst['op-status'],time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(inst['create-time'])),inst['vnfrs']])
+        table.align='l'
+        print(table)
+
+
+    def list_vnfr(self):
+        return self.get_vnfr_catalog()
+
+    def list_vnf_catalog(self):
+        resp = self.get_vnf_catalog()
+        table=PrettyTable(['VNFD Name','Id'])
+        for ns in resp['vnfd:vnfd']:
+            table.add_row([ns['name'],ns['id']])
+        table.align='l'
+        print(table)
---
+++ riftclient/scripts/rwclient.py
@@ -0,0 +1,118 @@
+import click
+from riftclient.common import manoRift
+from prettytable import PrettyTable
+import pprint
+import textwrap
+
+class SvMano(object):
+    def __init__(self,hostname):
+        self._hostname=hostname
+
+@click.group()
+@click.option('--hostname',envvar='RWCLIENT_RIFT_HOSTNAME',help='hostname of server.  Also can set RWCLIENT_RIFT_HOSTNAME in environment')
+@click.pass_context
+def cli(ctx,hostname):
+    ctx.obj=manoRift.ManoRift(hostname)
+
+@cli.command(name='ns-list')
+@click.pass_context
+def ns_list(ctx):
+    ctx.obj.list_ns_instance()
+
+@cli.command(name='nsd-list')
+@click.pass_context
+def nsd_list(ctx):
+    ctx.obj.list_ns_catalog()
+
+@cli.command(name='vnfd-list')
+@click.pass_context
+def vnfd_list(ctx):
+    ctx.obj.list_vnf_catalog()
+
+@cli.command(name='vnf-list')
+@click.pass_context
+def vnf_list(ctx):
+    resp=ctx.obj.list_vnfr()
+    table=PrettyTable(['VNFD Name','Id','Operational Status','Config Status','Mgmt interface','NSR Id'])
+    for vnfr in resp['vnfr:vnfr']:
+        table.add_row([vnfr['name'],vnfr['id'],vnfr['operational-status'],vnfr['config-status'],vnfr['mgmt-interface']['ip-address'],vnfr['nsr-id-ref']])
+    table.align='l'
+    print(table)
+
+@cli.command(name='ns-create')
+@click.argument('nsd_name')
+@click.argument('ns_name')
+@click.argument('vim_account')
+@click.option('--admin_status',default='ENABLED',help='administration status')
+@click.option('--ssh_keys',default='',help='comma separated list of keys to inject to vnfs')
+@click.option('--vim_network_prefix',default='',help='vim network name prefix')
+@click.pass_context
+def ns_create(ctx,nsd_name,ns_name,vim_account,admin_status,ssh_keys,vim_network_prefix):
+    ctx.obj.instantiate_ns(nsd_name,ns_name,vim_network_prefix=vim_network_prefix,ssh_keys=ssh_keys,account=vim_account)
+
+@cli.command(name='ns-delete')
+@click.argument('ns_name')
+@click.pass_context
+def ns_delete(ctx,ns_name):
+    ctx.obj.terminate_ns(ns_name)
+
+@cli.command(name='keypair-list')
+@click.pass_context
+def keypair_list(ctx):
+    resp=ctx.obj.list_key_pair()
+    table=PrettyTable(['Key Name','Key'])
+    for kp in resp:
+        table.add_row([kp['name'],kp['key']])
+    table.align='l'
+    print(table)
+
+@cli.command(name='upload-package')
+@click.argument('filename')
+@click.pass_context
+def upload_package(ctx,filename):
+    ctx.obj.upload_package(filename)
+
+@cli.command(name='ns-show')
+@click.argument('ns_name')
+@click.pass_context
+def ns_show(ctx,ns_name):
+    ctx.obj.show_ns(ns_name)
+
+@cli.command(name='ns-scaling-show')
+@click.argument('ns_name')
+@click.pass_context
+def show_ns_scaling(ctx,ns_name):
+    ctx.obj.show_ns_scaling(ns_name)
+
+@cli.command(name='nsd-delete')
+@click.argument('nsd_name')
+@click.pass_context
+def nsd_delete(ctx,nsd_name):
+    ctx.obj.delete_nsd(nsd_name)
+
+@cli.command(name='vim-create')
+@click.argument('name')
+@click.argument('user')
+@click.argument('password')
+@click.argument('auth_url')
+@click.argument('tenant')
+@click.argument('mgmt_network')
+@click.argument('floating_ip_pool')
+@click.option('--account_type',default='openstack')
+@click.pass_context
+def vim_create(ctx,name,user,password,auth_url,tenant,mgmt_network,floating_ip_pool,account_type):
+    ctx.obj.add_vim_account(name,user,password,auth_url,tenant,mgmt_network,floating_ip_pool,account_type)
+
+@cli.command(name='vim-list')
+@click.pass_context
+def vim_list(ctx):
+    resp=ctx.obj.list_vim_accounts()
+    table=PrettyTable(['Vim Name','User','Mgmt Network','Floating IP Pool','Connection Status'])
+    for account in resp:
+        table.add_row([account['name'],account[account['account-type']]['key'],account[account['account-type']]['mgmt-network'],account[account['account-type']]['floating-ip-pool'], account['connection-status']])
+    table.align='l'
+    print(table)
+
+
+if __name__ == '__main__':
+    cli()
